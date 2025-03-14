ba0346a0e3c96adb3e51829088b4b74eb0e8f3a5
1234 <abc@gmail.com>
2025-03-14 18:39:09 -0500

    This progrm will convert rupes to a dollar l add the function

---
+++ ConvertCurrency.java
@@ -0,0 +1,16 @@
+// java program to converst from 
+// rupee to the dollar and vice-versa
+// using Java Swing
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class ConvertCurrency
+{
+    // function to convert from rupes
+    // to the dollar using Java Swing
+    public static void converter()
+    {
+    
+    }
+}
